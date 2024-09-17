58a9cc3eb6dcd0f9f72575a3e5ab68f59dc57086
Methuselah <dstengac@gmail.com>
2024-09-17 20:24:49 +0200

    clear everything

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const btn = document.querySelector(".btn__add"),
+  input = document.querySelector(".input"),
+  option = document.querySelector("option"),
+  datalist = document.querySelector("#history");
 
+btn.addEventListener("click", () => {
+  newOption = option.cloneNode(true);
+  newOption.value = input.value;
+  datalist.appendChild(newOption);
+});
